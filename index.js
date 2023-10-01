7504b9c423d989c8915ff7695cf2d25ef500ec6e
Fith Wahome <wahomeimani@gmail.com>
2023-10-01 19:46:14 +0300

    assignment

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Convert the response to JSON
+    })
+    .then((data) => {
+      // Call the renderBooks function with the JSON data
+      renderBooks(data);
+    })
+    
+}
+
+// Call the fetchBooks function when the HTML document is loaded
+document.addEventListener('DOMContentLoaded', function () {
+  fetchBooks();
+});
+
+
+
+
+
+
   // To pass the tests, don't forget to return your fetch!
   
-}
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
